62a58ad3d128cf0c11c3d2cf30be3c76e09e70ed
ATI <commit@atifans.net>
2017-05-22 22:49:38 +0800

    prevent trashes in VARDATA() sneak into strtoull()

--- pg_phash--1.0.sql
+++ pg_phash--1.0.sql
@@ -1,1 +1,1 @@
-CREATE FUNCTION phash_hamming (signature1 varchar, signature2 varchar) RETURNS int AS '$libdir/pg_phash' LANGUAGE C IMMUTABLE STRICT;
+CREATE FUNCTION phash_hamming (signature1 varchar, signature2 varchar) RETURNS int AS '$libdir/pg_phash' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
--- pg_phash.c
+++ pg_phash.c
@@ -1,5 +1,6 @@
 #include <postgres.h>
 #include <fmgr.h>
+#include <stdlib.h>
 #include <string.h>
 #include <utils/datum.h>
 
@@ -13,21 +14,37 @@ typedef unsigned long long ulong64;
 PG_MODULE_MAGIC;
 #endif
 
-int32 ph_hamming_distance(const ulong64 hash1,const ulong64 hash2);
+int32 ph_hamming_distance(const ulong64 hash1, const ulong64 hash2);
 
 PG_FUNCTION_INFO_V1(phash_hamming);
 Datum phash_hamming(PG_FUNCTION_ARGS) {
-    // Prevent null values, memory address 0x00 will cause segfault
-    if (PG_GETARG_DATUM(0) == 0x00 || PG_GETARG_DATUM(1) == 0x00) {
+    // Prevent null pointers, which will cause segfault
+    if (PG_GETARG_DATUM(0) == NULL || PG_GETARG_DATUM(1) == NULL) {
         PG_RETURN_NULL();
     }
 
-    VarChar *varchar1 = PG_GETARG_VARCHAR_P(0);
-    VarChar *varchar2 = PG_GETARG_VARCHAR_P(1);
+    // Get varchar pointers
+    VarChar *ptr1 = PG_GETARG_VARCHAR_P(0);
+    VarChar *ptr2 = PG_GETARG_VARCHAR_P(1);
 
-    ulong64 long1 = strtoull(VARDATA(varchar1), NULL, 10);
-    ulong64 long2 = strtoull(VARDATA(varchar2), NULL, 10);
+    // Get varchar sizes
+    char size1 = VARSIZE(ptr1) - VARHDRSZ;
+    char size2 = VARSIZE(ptr2) - VARHDRSZ;
 
+    // Extract content to new memory space
+    char varchar1[size1 + 1];
+    memcpy(varchar1, (void*) VARDATA(ptr1), size1);
+    varchar1[size1] = '\0';
+
+    char varchar2[size2 + 1];
+    memcpy(varchar2, (void*) VARDATA(ptr2), size2);
+    varchar2[size2] = '\0';
+
+    // Convert number strings to ulong64
+    ulong64 long1 = strtoull(varchar1, NULL, 10);
+    ulong64 long2 = strtoull(varchar2, NULL, 10);
+
+    // Compute hamming distance
     int32 ret = ph_hamming_distance(long1, long2);
 
     PG_RETURN_INT32(ret);
@@ -55,7 +72,7 @@ Datum phash_hamming(PG_FUNCTION_ARGS) {
     D Grant Starkweather - dstarkweather@phash.org
 */
 
-int32 ph_hamming_distance(const ulong64 hash1,const ulong64 hash2){
+int32 ph_hamming_distance(const ulong64 hash1, const ulong64 hash2){
     ulong64 x = hash1^hash2;
     const ulong64 m1  = 0x5555555555555555ULL;
     const ulong64 m2  = 0x3333333333333333ULL;

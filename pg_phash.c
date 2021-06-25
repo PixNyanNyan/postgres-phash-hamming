4b9b69c41acd75be1fe4e6d91f1117622d2c77f2
Sh Lin <3275467+atitan@users.noreply.github.com>
2021-06-25 16:27:56 +0800

    Update pg_phash.c

--- pg_phash.c
+++ pg_phash.c
@@ -42,11 +42,11 @@ Datum phash_hamming(PG_FUNCTION_ARGS) {
     size2 = VARSIZE(ptr2) - VARHDRSZ;
 
     // Extract content to new memory space
-    varchar1 = malloc(sizeof (char) * size1);
+    varchar1 = malloc(sizeof (char) * (size1 + 1));
     memcpy(varchar1, (void*) VARDATA(ptr1), size1);
     varchar1[size1] = '\0';
 
-    varchar2 = malloc(sizeof (char) * size2);
+    varchar2 = malloc(sizeof (char) * (size2 + 1));
     memcpy(varchar2, (void*) VARDATA(ptr2), size2);
     varchar2[size2] = '\0';
 
